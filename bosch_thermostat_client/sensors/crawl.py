f4dab53396884f15f7c263d09dad216a24831860
Christoph Pech <christoph.pech@gmail.com>
2022-09-24 11:07:39 +0200

    Update crawl.py
    syntax

--- bosch_thermostat_client/sensors/crawl.py
+++ bosch_thermostat_client/sensors/crawl.py
@@ -34,7 +34,7 @@ class CrawlSensor(Sensor):
         """Update info about Crawl Sensor asynchronously."""
 
         def process_result(result):
-            if result is None
+            if result is None:
                 return 0
             if len(result) == 1:
                 for key, value in result[0].items():
